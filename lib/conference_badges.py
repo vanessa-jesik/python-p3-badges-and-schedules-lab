7936892a643fa80ef78bca1d2b0fa1493e4a904f
Vanessa Jesik <vajesik@gmail.com>
2023-09-18 16:16:10 -0600

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
+    # badge_list = []
+    # for name in names:
+    #     badge_list.append(f"Hello, my name is {name}.")
+    # return badge_list
+
 
 def assign_rooms(names):
-    return None
+    return [
+        f"Hello, {name}! You'll be assigned to room {index+1}!"
+        for index, name in enumerate(names)
+    ]
+    # room_list = []
+    # for index, name in enumerate(names):
+    #     room_list.append(f"Hello, {name}! You'll be assigned to room {index+1}!")
+    # return room_list
+
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for room in assign_rooms(names):
+        print(room)
